cb398ddab142ac8a8e51108862c4680b27b6b0bb
m-sparsh <msparsh0044@gmail.com>
2025-02-03 12:04:10 +0530

    signin/signup form & login/header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-red-500">
-      
-       hello world
-    
-    </div>
+   <Body />
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Header from "./Header";
+
+const Login = () => {
+  const [isSignInForm, setIsSignForm] = useState(true);
+
+  const toggleSigninForm = () => {
+    setIsSignForm(!isSignInForm);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_small.jpg"
+          alt="bg-img"
+        />
+      </div>
+      <form className="absolute p-12 bg-black w-4/12 mt-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email address"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <button className="p-3 my-6 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSigninForm}>
+          {isSignInForm
+            ? "New to netflix? Sign up now"
+            : "Already registered? Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
